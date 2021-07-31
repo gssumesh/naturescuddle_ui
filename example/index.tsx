c41f01ab7367e90e8692643fdafda6ca7a3b7837
gssumesh <gs.sumesh@gmail.com>
2021-07-31 11:47:53 +1000

    feat: releasing first version of launching page

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing, Button } from '../.';
+import { PageHomeComingSoon } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
-      <Button text="test" />
+      <PageHomeComingSoon />
     </div>
   );
 };
